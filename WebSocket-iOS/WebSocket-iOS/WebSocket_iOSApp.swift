dd88b7b614108874e131db368be50f83abbd0e89
Gökalp Gürocak <ggurocak58@gmail.com>
2026-02-22 14:24:48 +0300

    Add initial projects

---
+++ WebSocket-iOS/WebSocket-iOS/WebSocket_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebSocket_iOSApp.swift
+//  WebSocket-iOS
+//
+//  Created by Gökalp Gürocak on 22.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct WebSocket_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WebSocket-iOS/WebSocket-iOSTests/WebSocket_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  WebSocket_iOSTests.swift
+//  WebSocket-iOSTests
+//
+//  Created by Gökalp Gürocak on 22.02.2026.
+//
+
+import Testing
+@testable import WebSocket_iOS
+
+struct WebSocket_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
